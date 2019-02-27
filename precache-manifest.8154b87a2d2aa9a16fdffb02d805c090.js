b5dc7344225788ab3edf0540684952a16116081f
Jianshi Shen <jshe0550@uni.sydney.edu.au>
2019-02-27 20:02:24 +1100

    Updates

---
+++ precache-manifest.8154b87a2d2aa9a16fdffb02d805c090.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ddb6fe6bafbab15724c7304736d58c5",
+    "url": "/WeatherAPP/static/media/lightning.9ddb6fe6.jpg"
+  },
+  {
+    "revision": "d77839ffbb4d72580920",
+    "url": "/WeatherAPP/static/css/main.a952a449.chunk.css"
+  },
+  {
+    "revision": "3dda21d9397864fce288",
+    "url": "/WeatherAPP/static/js/runtime~main.3dda21d9.js"
+  },
+  {
+    "revision": "ed70a49d19f760322c64",
+    "url": "/WeatherAPP/static/js/2.ed70a49d.chunk.js"
+  },
+  {
+    "revision": "cea21cb0213666191e2c561a3ea874a0",
+    "url": "/WeatherAPP/static/media/sunny.cea21cb0.jpg"
+  },
+  {
+    "revision": "24b05d022600f0064ab963847fb77970",
+    "url": "/WeatherAPP/static/media/cloudy.24b05d02.jpg"
+  },
+  {
+    "revision": "d77839ffbb4d72580920",
+    "url": "/WeatherAPP/static/js/main.d77839ff.chunk.js"
+  },
+  {
+    "revision": "dff305838bcf306c7a47c373365dd150",
+    "url": "/WeatherAPP/static/media/mist.dff30583.jpg"
+  },
+  {
+    "revision": "5e7460de5edae75bbb359ee321b66a2f",
+    "url": "/WeatherAPP/static/media/snowy.5e7460de.jpg"
+  },
+  {
+    "revision": "bbf6726e0f1446159c2eea9b11758608",
+    "url": "/WeatherAPP/static/media/overcast.bbf6726e.jpg"
+  },
+  {
+    "revision": "f22feda63f932d819fb95be100f0e7fe",
+    "url": "/WeatherAPP/static/media/rainy.f22feda6.jpg"
+  },
+  {
+    "revision": "2abcaf5e99ae4b34bf040cd5a84e28a5",
+    "url": "/WeatherAPP/index.html"
+  }
+];
